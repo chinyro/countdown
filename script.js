967dda53b91a50207a023a3e1baebcd31a3e18b6
sydrob <55166840+sydrobhez@users.noreply.github.com>
2023-03-13 19:25:51 -0600

    updated event listeners

--- script.js
+++ script.js
@@ -2,6 +2,22 @@ const inputContainer = document.getElementById('input-container'); //show and hi
 const countdownForm = document.getElementById('countdownForm'); //get the results from input fields
 const dateEl = document.getElementById('date-picker'); //show and hide container
 
+let countDownTitle = '';
+let countDownDate = '';
+
 // Set Date input min with today's date
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
+
+// Value from form input
+function updateCountdown(event) {
+    event.preventDefault();
+    countDownTitle = event.srcElement[0].value;
+    countDownDate = event.srcElement[1].value;
+    console.log(countDownTitle, countDownDate);
+}
+
+// Event Listeners
+countdownForm.addEventListener('submit', updateCountdown);
+
+
